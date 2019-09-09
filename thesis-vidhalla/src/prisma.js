d9a1b2c864d46e4c651fa3b6bcdb5253eed2aa69
borisStanojevic <borisstanojevicfkkt@gmail.com>
2019-09-09 22:20:15 +0200

    Core model finished

--- thesis-vidhalla/src/prisma.js
+++ thesis-vidhalla/src/prisma.js
@@ -7,6 +7,47 @@ const prisma = new Prisma({
 
 //prisma.query prisma.mutation prisma.subscription prisma.exists
 //dva argumenta : operation arguments i selection set
-prisma.query.users(null, "{ id name videos { id title url} }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.users(null, "{ id name videos { id title url} }").then(data => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// prisma.mutation
+//   .createUser(
+//     {
+//       data: { username: "pera123", email: "pera@email.com" }
+//     },
+//     "{ id username }"
+//   )
+//   .then(data => {
+//     console.log(data, undefined, 2);
+//   });
+
+//Definisemo fuknciju
+const createVideoForUser = async (uploaderId, data) => {
+  const video = await prisma.mutation.createVideo(
+    {
+      data: {
+        ...data,
+        uploader: {
+          connect: {
+            id: uploaderId
+          }
+        }
+      }
+    },
+    "{ id }"
+  );
+
+  const uploader = await prisma.query.user(
+    { where: { id: uploaderId } },
+    "{ id username videos { id url title } }"
+  );
+
+  return uploader;
+};
+
+//Zatim pozivamo sa argumentima
+// createVideoForUser(...).then(user => console.log(JSON.stringify(user, undefined, 2)))
+prisma.query.users(null, "{ id name videos { id title url} }");
+
+
